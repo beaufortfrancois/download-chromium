7dae0b55ad86882fc226734ee45eecc9cb045283
Francois Beaufort <beaufort.francois@gmail.com>
2012-07-12 11:14:18 +0200

    Initial landing without any modifications to the first version

---
+++ main.py
@@ -0,0 +1,87 @@
+import datetime
+import os
+
+from google.appengine.ext import db, webapp
+from google.appengine.ext.webapp import template, util
+
+from utils import find_platform, get_platform, WINDOWS, MAC, LINUX, LINUX_X64
+
+
+class Download(db.Model):
+    platform = db.StringProperty()
+    is_hacked = db.BooleanProperty()
+    date = db.DateTimeProperty(auto_now_add=True)
+
+class PlatformNotDetected(db.Model):
+    user_agent = db .StringProperty()
+
+
+class DownloadHandler(webapp.RequestHandler):
+    def get(self, platform_name):
+        referer = self.request.headers.get('Referer')
+        is_hacked = not bool(referer)
+        platform = get_platform(platform_name)
+        if not platform:
+            return self.redirect('http://www.youtube.com/e/o_asQwJqWCI?autoplay=1&start=16')
+
+        download = Download(platform=platform.name, is_hacked=is_hacked)
+        download.put()
+        self.redirect(platform.get_last_snapshot_url())
+
+class TrendsHandler(webapp.RequestHandler):
+    def get(self):
+        try:
+            days = int(self.request.get('days', 7))
+        except ValueError:
+            days = 7
+
+        win_count = Download.all().filter('date >', (datetime.datetime.now() - datetime.timedelta(days=days))).filter('platform =', WINDOWS.name).count(limit=None)
+        mac_count = Download.all().filter('date >', (datetime.datetime.now() - datetime.timedelta(days=days))).filter('platform =', MAC.name).count(limit=None)
+        linux_count = Download.all().filter('date >', (datetime.datetime.now() - datetime.timedelta(days=days))).filter('platform =', LINUX.name).count(limit=None)
+        linux_x64_count = Download.all().filter('date >', (datetime.datetime.now() - datetime.timedelta(days=days))).filter('platform =', LINUX_X64.name).count(limit=None)
+        count = win_count + mac_count + linux_count
+
+        template_values = {
+            'win_count': win_count,
+            'mac_count': mac_count,
+            'linux_count': linux_count,
+            'linux_x64_count': linux_x64_count,
+            'days': days,
+            'count': count,
+        }
+        path = os.path.join(os.path.dirname(__file__), 'trends.html')
+        self.response.out.write(template.render(path, template_values))
+
+class IndexHandler(webapp.RequestHandler):
+    def get(self):
+        user_agent = self.request.headers['User-Agent']
+        platform = find_platform(user_agent)
+        if not platform and PlatformNotDetected.all().filter('user_agent =', user_agent).count() == 0:
+            platform_not_detected = PlatformNotDetected(user_agent=user_agent)
+            platform_not_detected.put()
+            from google.appengine.api import mail
+            mail.send_mail(
+                sender='DownloadChromium Notice <beaufort.francois@gmail.com>',
+                to='<beaufort.francois+download-chromium@gmail.com>',
+                subject='New Platform not detected',
+                body=platform_not_detected.user_agent,
+            )
+        template_values = {
+            'platform': platform,
+        }
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
+
+        self.response.out.write(template.render(path, template_values))
+
+
+def main():
+    application = webapp.WSGIApplication([
+        ('/', IndexHandler),
+        ('/dl/(.*)', DownloadHandler),
+        ('/trends/', TrendsHandler),
+    ], debug=True)
+    util.run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,58 @@
+import re
+
+from google.appengine.api import urlfetch
+
+
+LAST_REVISION_TEMPLATE = 'https://commondatastorage.googleapis.com/chromium-browser-snapshots/%(platform)s/LAST_CHANGE'
+LAST_SNAPSHOT_TEMPLATE = 'https://commondatastorage.googleapis.com/chromium-browser-snapshots/%(platform)s/%(revision)s/%(zip_name)s'
+
+
+platforms = []
+
+
+class ChromiumPlatform(object):
+    def __init__(self, name, pretty_name, zip_name):
+        self.name = name
+        self.pretty_name = pretty_name
+        self.zip_name = zip_name
+        self.last_revision_url = LAST_REVISION_TEMPLATE % {'platform' : self.name}
+        platforms.append(self)
+
+    def __repr__(self):
+        return self.name
+
+    def get_last_snapshot_url(self):
+        revision = urlfetch.fetch(self.last_revision_url).content
+
+        return LAST_SNAPSHOT_TEMPLATE % {
+            'platform': self.name,
+            'revision': revision,
+            'zip_name': self.zip_name,
+        }
+
+
+LINUX     = ChromiumPlatform('Linux',     'Linux',   'chrome-linux.zip')
+LINUX_X64 = ChromiumPlatform('Linux_x64', 'Linux',   'chrome-linux.zip')
+WINDOWS   = ChromiumPlatform('Win',       'Windows', 'public-read/chrome-win32.zip')
+MAC       = ChromiumPlatform('Mac',       'Mac',     'chrome-mac.zip')
+
+
+def find_platform(string):
+    if re.search('Win', string):
+        return WINDOWS
+    elif re.search('Mac', string):
+        return MAC
+    elif re.search('Linux', string):
+        if re.search('_64', string):
+            return LINUX_X64
+        else:
+            return LINUX
+    else:
+        return None
+
+
+def get_platform(name):
+    try:
+        return [p for p in platforms if p.name == name][0]
+    except IndexError:
+        return None
